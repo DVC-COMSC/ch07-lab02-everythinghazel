1d47cf58a6b4d4998a19888ad00c8092c892c0f1
everythinghazel <112515847+everythinghazel@users.noreply.github.com>
2022-11-22 20:26:39 +0000

    lab1 chpt7

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-
 # ******************************
-# Make your Code
+"""Module providing Function printing python version."""
+
+import random
+
+lst = []
+
+for i in range(10):
+    num = random.randint
+    lst.append(random.randint(0,100))
+print(lst.index(min(lst)))
+
+
+
+
+
+
 # ******************************
